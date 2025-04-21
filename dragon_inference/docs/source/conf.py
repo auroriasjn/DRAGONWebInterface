f5df1fb0c038f3690f53a63284669341aec3bf4f
Jeremy Ng <jeremy.ng@yale.edu>
2025-04-21 14:39:01 -0400

    Attempting readthedocs integration

--- dragon_inference/docs/source/conf.py
+++ dragon_inference/docs/source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 # Add your project root to sys.path
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../../..'))
 
 # Safe to import anything afterward
 from sphinxawesome_theme.postprocess import Icons
